884847c66935f6b543df4715be4e9acc39f75e3f
Vlad <Smirnov.ru92@mail.ru>
2024-01-12 18:45:46 +0500

    feat: create calendar

--- scripts/months.js
+++ scripts/months.js
@@ -40,12 +40,13 @@ let currentMonth = monthsRu[currentMonthIndex]
 let daysInMonth
 let firstMondayDate
 
-function getMonthData(year, month) {
-    const firstDayOfMonth = new Date(year, month, 1);
+function getMonthData() {
+    const firstDayOfMonth = new Date(currentYear, currentMonthIndex, 1,-1);
     daysInMonth = currentDate.daysInMonth()
     const dayOfWeek = firstDayOfMonth.getDay();
-    const daysToAdd = dayOfWeek === 0 ? 1 : 9 - dayOfWeek;
-    firstMondayDate = +(new Date(year, month - 1, daysToAdd).toString().split(' ')[2][1]);
+    const daysToAdd = dayOfWeek === 0 ? 1 : 8 - dayOfWeek;
+    console.log(new Date(2023, 0, daysToAdd).toString().split(' ')[2][1])
+    firstMondayDate = +(new Date(currentYear, currentMonthIndex - 1, daysToAdd).toString().split(' ')[2][1]);
     if (firstMondayDate === 8) {
         firstMondayDate = 1
     }
@@ -59,7 +60,6 @@ function updateDate() {
 
 
 const setCurrentDate = (to) => {
-
     if (to === 'today') {
         currentDate = new Date()
     } else {
@@ -69,88 +69,3 @@ const setCurrentDate = (to) => {
 }
 
 
-
-
-
-
-
-
-
-
-
-// Date.prototype.daysInMonth = function () {
-//     return 32 - new Date(this.getFullYear(), this.getMonth(), 32).getDate();
-// };
-//
-// const monthsRu = {
-//     0: 'Январь',
-//     1: 'Февраль',
-//     2: 'Март',
-//     3: 'Апрель',
-//     4: 'Май',
-//     5: 'Июнь',
-//     6: 'Июль',
-//     7: 'Август',
-//     8: 'Сентябрь',
-//     9: 'Октябрь',
-//     10: 'Ноябрь',
-//     11: 'Декабрь'
-// };
-//
-//
-// const monthsEn = {
-//     0: 'January',
-//     1: 'February',
-//     2: 'March',
-//     3: 'April',
-//     4: 'May',
-//     5: 'June',
-//     6: 'July',
-//     7: 'August',
-//     8: 'September',
-//     9: 'October',
-//     10: 'November',
-//     11: 'December'
-// };
-//
-// let currentDate = new Date()
-// let currentYear = currentDate.getUTCFullYear()
-// let currentMonthIndex = currentDate.getUTCMonth()
-// let currentMonth = monthsRu[currentMonthIndex]
-// let daysInMonth
-// let firstMondayDate
-//
-// function getMonthData(year, month) {
-//     const firstDayOfMonth = new Date(year, month, 1);
-//     daysInMonth = currentDate.daysInMonth()
-//     const dayOfWeek = firstDayOfMonth.getDay();
-//     const daysToAdd = dayOfWeek === 0 ? 1 : 9 - dayOfWeek;
-//     firstMondayDate = +(new Date(year, month - 1, daysToAdd).toString().split(' ')[2][1]);
-//     if (firstMondayDate === 8) {
-//         firstMondayDate = 1
-//     }
-// }
-//
-// function updateDate() {
-//     currentYear = currentDate.getUTCFullYear();
-//     currentMonthIndex = currentDate.getUTCMonth();
-//     currentMonth = monthsRu[currentMonthIndex];
-// }
-//
-//
-// const setCurrentDate = (to) => {
-//
-//     if (to === 'today') {
-//         currentDate = new Date()
-//     } else {
-//         currentDate.setMonth(currentDate.getMonth() + (to === 'next' ? +1 : -1))
-//     }
-//     updateDate()
-// }
-//
-//
-//
-//
-//
-//
-//
--- scripts/script.js
+++ scripts/script.js
@@ -5,26 +5,53 @@ const nextMonthButton = document.querySelector('.nextMonthButton')
 const prevMonthButton = document.querySelector('.prevMonthButton')
 const todayButton = document.querySelector('.today')
 
-nextMonthButton.addEventListener('click',()=>{
-    setCurrentDate('next')
+getMonthData()
+const changeMonthHandler = (arg) => {
+    setCurrentDate(arg)
     dateSpan.textContent = `${currentMonth} ${currentYear}`
-    getMonthData(currentYear,currentMonthIndex)
+    getMonthData()
     console.log(daysInMonth,firstMondayDate)
+}
+
+nextMonthButton.addEventListener('click',()=>{
+  changeMonthHandler('next')
+    foo()
 })
 
 prevMonthButton.addEventListener('click',()=>{
-    setCurrentDate('prev')
-    dateSpan.textContent = `${currentMonth} ${currentYear}`
-    getMonthData(currentYear,currentMonthIndex)
-    console.log(daysInMonth,firstMondayDate)
+    changeMonthHandler('prev')
+    foo()
 })
 
 todayButton.addEventListener('click',()=>{
-    setCurrentDate('today')
-    dateSpan.textContent = `${currentMonth} ${currentYear}`
-    getMonthData(currentYear,currentMonthIndex)
-    console.log(daysInMonth,firstMondayDate)
+    changeMonthHandler('today')
+    foo()
 })
+
+function foo(){
+
+    for(let i = 1; i<= 35; i++){
+        const el = document.getElementById(`${i}`)
+        const divEl = el.querySelector('div');
+        divEl.textContent = ''
+    }
+
+
+    for(let i = 1; i <= 35; i++){
+firstMondayDate++
+        // console.log(i)
+        // console.log(firstMondayDate)
+
+        if(firstMondayDate >= daysInMonth + 2) break
+
+        const el = document.getElementById(`${i}`)
+        const divEl = el.querySelector('div');
+        divEl.textContent = (firstMondayDate - 1).toString()
+        el.appendChild(divEl)
+    }
+}
+
+foo()
 
 
 
